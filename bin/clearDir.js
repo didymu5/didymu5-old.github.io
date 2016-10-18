98ebedf615f39d5ca000dfa8c9c1092b791d507e
tdubs <thomas.wu@kp.org>
2016-10-18 16:56:03 -0700

    added bin directory of random utility stuff

---
+++ bin/clearDir.js
@@ -0,0 +1,21 @@
+'use strict';
+import * as fs from "fs";
+
+const clearDir = function(dirPath) {
+  try {
+    var files = fs.readdirSync(dirPath);
+  } catch (e) {
+    return;
+  }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath);
+      else
+        rmDir(filePath);
+    }
+  fs.rmdirSync(dirPath);
+};
+
+export default clearDir;
